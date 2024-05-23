0ea2c9614f9d2af5a49a250fd61e7756704b28ac
Nawiss <lloydnawiss@gmail.com>
2024-05-23 22:06:48 +0200

    initialisation du projet without javascript lol

---
+++ index.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('btn');
+    button.addEventListener('click', () => {
+        alert('Bouton cliqué !');
+    });
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+html:5>head>title{Titre de la page}+body>h1{Bonjour tout le monde!}+button#btn{Clique moi}
